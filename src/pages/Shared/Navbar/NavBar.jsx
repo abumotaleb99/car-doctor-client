bb257753299193a4df80f5c1e254e71f63c1650e
Abu Motaleb <abumotaleb99@gmail.com>
2023-05-17 02:55:49 +0600

    added jwt

--- src/pages/Shared/Navbar/NavBar.jsx
+++ src/pages/Shared/Navbar/NavBar.jsx
@@ -7,7 +7,11 @@ const NavBar = () => {
   const { user, logOut } = useContext(AuthContext);
 
   const handleLogOut = () => {
-    logOut();
+    logOut()
+      .then(() => {
+        localStorage.removeItem("car-access-token");
+      })
+      .catch((error) => console.log(error));
   };
 
   const navItems = (
